d6e5cc6343808371e6f05f53c1eaa65b00e7265a
David <davidshinlee13@gmail.com>
2022-01-31 08:29:51 -0500

    second recenter

--- welcome.py
+++ welcome.py
@@ -18,7 +18,7 @@ while running:
 	fontObj = pygame.font.SysFont("arial", 200)
 	textSurfaceObj = fontObj.render('2048', True, variables.DEFAULT_DARK2, variables.DEFAULT)
 	textRectObj = textSurfaceObj.get_rect()
-	textRectObj.center  = (500, 400)
+	textRectObj.center  = (450, 300)
 	window.blit(textSurfaceObj, textRectObj)
 	pygame.display.update()
 	
