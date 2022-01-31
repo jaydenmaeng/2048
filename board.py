7c866bc733394e8baf348d514eb97cacef4e3604
Justin Hwang <advancedjus@gmail.com>
2022-01-31 08:45:51 -0500

    made board

--- board.py
+++ board.py
@@ -17,16 +17,18 @@ while running:
 	
 	# set background color 
 	window.fill(variables.DEFAULT)
-	fontObj = pygame.font.SysFont("arial", 50)
+	fontObj = pygame.font.SysFont("arial bold", 80)
 	textSurfaceObj = fontObj.render('2048', True, variables.DEFAULT_DARK2, variables.DEFAULT)
 	textRectObj = textSurfaceObj.get_rect()
-	textRectObj.center  = (100, 80)
+	textRectObj.center  = (380, 80)
 	window.blit(textSurfaceObj, textRectObj)
-	
+	board = pygame.Rect(285, 285, 500, 500)
+	pygame.draw.rect(window, variables.DEFAULT_DARK, board, 0, 0, 3, 3, 3, 3)
+
 	for x in range (0, 4):
 		for y in range (0, 4):
-			rect = pygame.Rect(300 + 115 * x, 300 + 115 * y, 100, 100)
-			pygame.draw.rect(window, variables.DEFAULT_DARK, rect, 0, 0, 3, 3, 3, 3)
+			rect = pygame.Rect(300 + 121.25 * x, 300 + 121.25 * y, 106.25, 106.25)
+			pygame.draw.rect(window, variables.DEFAULT_TAN, rect, 0, 0, 3, 3, 3, 3)
 	pygame.display.update()
 	
 	# update window
