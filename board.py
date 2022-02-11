a78421e7ca465e2e751879c438798006b66d25f2
Justin Hwang <advancedjus@gmail.com>
2022-02-11 08:41:12 -0500

    fixed error

--- board.py
+++ board.py
@@ -3,10 +3,10 @@ import variables
 import tile
 
 def main():
+		
 	pygame.init()
 	window = pygame.display.set_mode((1000, 800))
 	running = True
-
 	pygame.draw.rect(window, variables.DEFAULT_DARK, pygame.Rect(30, 30, 60, 60), 2, 3)
 
 	tile1 = tile.Tile(window)
@@ -14,7 +14,7 @@ def main():
 	# tile3 = tile.Tile(window, 32)
 	print(str(tile1.x) + " " + str(tile1.y))
 	print(str(tile2.x) + " " + str(tile2.y))
-		
+
 	while running:
 
 		# check for event if user has pushed any event in queue
@@ -25,7 +25,7 @@ def main():
 			if event.type == pygame.K_LEFT:
 				tile.leftArrow()
 		
-		# set background color 
+			# set background color 
 		window.fill(variables.DEFAULT)
 		fontObj = pygame.font.SysFont("arial bold", 80)
 		textSurfaceObj = fontObj.render('2048', True, variables.DEFAULT_DARK2, variables.DEFAULT)
@@ -34,32 +34,35 @@ def main():
 		window.blit(textSurfaceObj, textRectObj)
 		board = pygame.Rect(285, 285, 500, 500)
 		pygame.draw.rect(window, variables.DEFAULT_DARK, board, 0, 0, 3, 3, 3, 3)
-		
+
 		for x in range (0, 4):
 			for y in range (0, 4):
 				rect = pygame.Rect(300 + 121.25 * x, 300 + 121.25 * y, 106.25, 106.25)
 				pygame.draw.rect(window, variables.DEFAULT_TAN, rect, 0, 0, 3, 3, 3, 3)
-	
-	# set background color 
-	window.fill(variables.DEFAULT)
-	fontObj = pygame.font.SysFont("arial", 75)
-	textSurfaceObj = fontObj.render('2048', True, variables.DEFAULT_DARK2, variables.DEFAULT)
-	textRectObj = textSurfaceObj.get_rect()
-	textRectObj.center  = (100, 80)
-	window.blit(textSurfaceObj, textRectObj)
-	
-	pygame.draw.rect(window, variables.DEFAULT_TAN, pygame.Rect(675, 100, 110, 60), 0, 0, 3, 3, 3, 3)
-	fontObj2 = pygame.font.SysFont("arial bold", 25)
-	textSurfaceObj2 = fontObj2.render('Best', True, variables.DEFAULT_DARK2, variables.DEFAULT_TAN)
-	textRectObj2 = textSurfaceObj2.get_rect()
-	textRectObj2.center  = (730, 115)
-	window.blit(textSurfaceObj2, textRectObj2)
 
-	pygame.display.update()
+		# Drawing Rectangle
+		pygame.draw.rect(window, variables.DEFAULT_TAN, pygame.Rect(590, 100, 80, 60), 0, 0, 3, 3, 3, 3)
+		fontObj2 = pygame.font.SysFont("arial bold", 20)
+		textSurfaceObj2 = fontObj2.render('Score:', True, variables.DEFAULT_DARK2, variables.DEFAULT_TAN)
+		textRectObj2 = textSurfaceObj2.get_rect()
+		textRectObj2.center  = (610, 130)
+		window.blit(textSurfaceObj2, textRectObj2)
+
+		pygame.draw.rect(window, variables.DEFAULT_TAN, pygame.Rect(680, 100, 100, 60), 0, 0, 3, 3, 3, 3)
+		fontObj2 = pygame.font.SysFont("arial bold", 20)
+		textSurfaceObj2 = fontObj2.render('Best:', True, variables.DEFAULT_DARK2, variables.DEFAULT_TAN)
+		textRectObj2 = textSurfaceObj2.get_rect()
+		textRectObj2.center  = (700, 130)
+		window.blit(textSurfaceObj2, textRectObj2)
+
+		# spawn two tiles
+		tile1.display()
+		tile2.display()
+
+		pygame.display.update()
+			
+		# update window 
+		pygame.display.flip()
+	pygame.quit()
 		
-	# update window 
-	pygame.display.flip()
-		
-
-
 main()
