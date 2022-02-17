1e9b12ab9f87b782102e46c51ca18f0d81b37807
Justin Hwang <advancedjus@gmail.com>
2022-02-16 23:15:08 -0500

    welcome

--- welcome.py
+++ welcome.py
@@ -24,22 +24,21 @@ while running:
 	
 	# set background color 
 	window.fill(variables.DEFAULT)
-	fontObj = pygame.font.SysFont("times new roman", 200)
-	textSurfaceObj = fontObj.render('2048', True, variables.DEFAULT_DARK2, variables.DEFAULT)
-	textRectObj = textSurfaceObj.get_rect()
-	textRectObj.center  = (490, 250)
-	window.blit(textSurfaceObj, textRectObj)
+
+	image = pygame.image.load("assets/logo.png")
+	window.blit(image, (280, 160))
 
 	fontObj2 = pygame.font.SysFont("arial", 25)
 	textSurfaceObj2 = fontObj2.render('By Justin Hwang, David Lee, Jayden Maeng, & Vincent Kim', True, variables.DEFAULT_DARK2, variables.DEFAULT)
 	textRectObj2 = textSurfaceObj2.get_rect()
-	textRectObj2.center  = (500, 395)
+	textRectObj2.center  = (500, 380)
 	window.blit(textSurfaceObj2, textRectObj2)
 
 	fontObj3 = pygame.font.SysFont("arial bold", 40)
 	textSurfaceObj3 = fontObj3.render('Start', True, variables.DEFAULT_DARK2, variables.DEFAULT_TAN)
+	pygame.draw.rect(window, variables.DEFAULT_TAN, pygame.Rect(450, 430, 100, 60), 0, 0, 3, 3, 3, 3)
 	textRectObj3 = textSurfaceObj3.get_rect()
-	textRectObj3.center  = (500, 470)
+	textRectObj3.center  = (500, 460)
 	window.blit(textSurfaceObj3, textRectObj3)
 
 	pygame.display.update()
